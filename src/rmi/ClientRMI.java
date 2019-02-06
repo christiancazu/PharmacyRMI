cb3fb78141a52eb83cdfc01ffb2109023e7a0461
christiancazu@gmail.com <30902930+christiancazu@users.noreply.github.com>
2019-02-06 15:53:54 -0500

    serch by name product

--- src/rmi/ClientRMI.java
+++ src/rmi/ClientRMI.java
@@ -6,6 +6,8 @@ import java.rmi.RemoteException;
 import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
 import java.util.List;
+import java.util.Optional;
+import java.util.Scanner;
 import services.ProductService;
 
 /**
@@ -14,16 +16,37 @@ import services.ProductService;
  */
 public class ClientRMI {
     public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
         ProductService productService;
         String serverIpAddress = "localhost";
-        int serverPort = 8888;
+        int serverPort = 8888, id;
         try {
             Registry registry = LocateRegistry.getRegistry(serverIpAddress, serverPort);
             productService = (ProductService) (registry.lookup("ServerRMI"));        
-        
-            List<Product> received = productService.findProduct();
-            String showMenu = productService.showMenu();
-            System.out.println(showMenu);
+            do {       
+                System.out.println(productService.showMenu());
+                
+                System.out.println("Find product name : ...");
+                List<Product> products = productService.findProduct(scan.nextLine());
+                
+                id = 1;
+                if (products.size() == 0) {
+                    System.out.println("No products found ...");
+                } else {
+                    for(Product product : products){
+                    System.out.print(id++ + "- " + 
+                            product.getName()+ "\t\t" + 
+                            product.getPrice() + "\t" + 
+                            product.getStock());
+                    System.out.println();
+                    for (int i = 0; i < 48; i++) {
+                        System.out.print("-");
+                    }
+                    System.out.println();
+                    }
+                }
+                
+            } while (true);
         } catch (RemoteException | NotBoundException  e) {
         }        
     }
